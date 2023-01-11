18357d8dba926a852c4151363e7c8fe60d184690
aimansae <saeedaimann@gmail.com>
2023-01-11 18:25:02 +0000

    Commit

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
-
+import Loader from './Loader'
 export class Content extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            isLoaded: false,
+        }
     }
-    
+    componentDidMount() {
+        setTimeout(() =>{
+            this.setState({
+                isLoaded:true,
+            })
+        },2000)
+    }
     render() {
         return (
             <div className={css.Content}>
@@ -33,8 +42,12 @@ export class Content extends Component {
 
 
                     {/* Part 2: Creating a child component */}
-
+                    {this.state.isLoaded ?
+                    
                     <PostItem savedPosts={savedPosts} />
+                    : <Loader/>
+                }
+                    
                 </div>
             </div>
         )
